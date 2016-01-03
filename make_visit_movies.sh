32a44c1b4aab1da45b24f00a4bf31e8cf665c797
Elias Rubin <erubin@princeton.edu>
2016-01-03 03:58:03 -0900

    fixed bash script to play nicely with TigressData

--- make_visit_movies.sh
+++ make_visit_movies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #rm frames/*density*.ppm
-visit -cli -nowin -s movie_density.py
+mkdir frames
+visit -cli -nowin -s density_movie.py
 ls frames/*density*.ppm > frames/file_list
 rm *density*.fli
 ppm2fli -v -s25 -g460x783 frames/file_list isoSelfG_run.fli
